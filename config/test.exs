362466eaf749c07317dc1ad9e0e5681f947cac66
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2021-03-07 18:22:54 +0300

    test config

--- config/test.exs
+++ config/test.exs
@@ -5,11 +5,12 @@ import Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
+
 config :doto, Doto.Repo,
   username: "postgres",
-  password: "postgres",
+  password: System.get_env("DB_PASSWORD") || "localhost",
   database: "doto_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
